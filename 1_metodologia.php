5f88dc3125c6948709874f7c8f090e15e8ce2d48
luiseduarter <luiseduarter@gmail.com>
2022-08-07 11:21:20 -0500

    tareaMetodologia

--- 1_metodologia.php
+++ 1_metodologia.php
@@ -1,2 +1,19 @@
 <?php
 echo "proyecto de meotdologia\n";
+
+class A
+{
+private $:foobar;
+function __construct($foobar)
+{
+
+31
+
+$this->nombre = $foobar;
+}
+function __toString()
+{
+return sprintf ("Hola, %s.\n", $this->foobar);
+}
+}Añado la clase al archivo que estamnso traba
+
